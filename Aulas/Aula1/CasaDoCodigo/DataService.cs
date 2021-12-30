24350f247659d78cfe650da5e6521833c05da3c2
Leandro Oliveira <leandro.oliveira@adm.unip.br>
2021-12-30 18:21:50 -0300

    iniciando o repositorio

---
+++ Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -0,0 +1,37 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    public partial class Startup
+    {
+        class DataService : IDataService
+        {
+            private readonly ApplicationContext contexto;
+            private readonly IProdutoRepository produtoRepository;
+
+            public DataService(ApplicationContext contexto, IProdutoRepository produtoRepository)
+            {
+                this.contexto = contexto;
+                this.produtoRepository = produtoRepository;
+            }
+
+            public void InicializaDB()
+            {
+                contexto.Database.EnsureCreated();
+                List<Livro> livors = GetLivros();
+                produtoRepository.SaveProdutos(livors);
+            }            
+
+            private static List<Livro> GetLivros()
+            {
+                var json = File.ReadAllText("livros.json");
+                var livors = JsonConvert.DeserializeObject<List<Livro>>(json);
+                return livors;
+            }
+        }
+    }
+}
---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/IProdutoRepository.cs
@@ -0,0 +1,11 @@
+﻿using CasaDoCodigo.Models;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IProdutoRepository
+    {
+        void SaveProdutos(List<Livro> livors);
+        IList<Produto> GetProdutos();
+    }
+}
